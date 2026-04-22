e88ff9d0af086e3c142ce1bf135a81ad9a8e319c
Brooke Farlow <farlowbrooke@gmail.com>
2026-04-22 12:18:29 -0400

    portfolio page

---
+++ lab12-recap/js/script.js
@@ -0,0 +1,13 @@
+console.log("Javascript is connected!");
+
+// Open the overlay
+function openOverlay() {}
+    document.getElementById("overlay-menu").style.width = "100vw";
+
+// Close the overlay
+function closeOverlay() {
+    document.getElementById("overlay-menu").style.display = "none";
+}
+
+// Add event listener to the menu icon
+function
